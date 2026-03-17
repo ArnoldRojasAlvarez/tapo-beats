817022b28a5ba14ecbc940d001eaea3507170b0d
Artox <131567196+ArnoldRojasAlvarez@users.noreply.github.com>
2026-03-17 12:26:07 -0600

    Use static ngrok domain in startup script

--- start_tapobeats.bat
+++ start_tapobeats.bat
@@ -13,9 +13,8 @@ start /B python -m src.main serve
 :: Wait for Flask to be ready
 timeout /t 3 /nobreak >nul
 
-:: Start ngrok (use static domain if available, otherwise dynamic)
-:: To use a free static domain, run: ngrok http 5000 --domain YOUR_DOMAIN.ngrok-free.app
-start /B ngrok http 5000
+:: Start ngrok with static domain (URL never changes)
+start /B ngrok http 5000 --domain YOUR_NGROK_DOMAIN_HERE
 
 echo TapoBeats is running!
 echo Flask: http://localhost:5000
